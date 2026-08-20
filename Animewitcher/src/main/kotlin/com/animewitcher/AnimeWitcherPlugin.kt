2f515fd8ae637715d9bd4f36bdf92e131244114f
Leaf <xoxobrain@gmail.com>
2026-08-20 16:58:43 +0100

    Remove comments from AnimeWitcherPlugin.kt

--- Animewitcher/src/main/kotlin/com/animewitcher/AnimeWitcherPlugin.kt
+++ Animewitcher/src/main/kotlin/com/animewitcher/AnimeWitcherPlugin.kt
@@ -8,13 +8,10 @@ import com.lagradost.cloudstream3.plugins.Plugin
 @CloudstreamPlugin
 class AnimeWitcherPlugin : Plugin() {
     override fun load(context: Context) {
-        // 1. Register the provider
         registerMainAPI(AnimeWitcherProvider())
 
-        // 2. Get SharedPreferences for our settings
         val sharedPref = context.getSharedPreferences("AnimeWitcherPrefs", Context.MODE_PRIVATE)
 
-        // 3. Hook up the settings UI
         openSettings = { ctx ->
             val activity = ctx as? AppCompatActivity
             if (activity != null) {
