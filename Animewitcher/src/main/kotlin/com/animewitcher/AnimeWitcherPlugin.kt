dd71fa822861416784a01320ec3ff3d621122124
Leaf <xoxobrain@gmail.com>
2026-08-20 16:16:00 +0100

    Remove proxy host setting from plugin
    Removed proxy host setting from AnimeWitcherPlugin.

--- Animewitcher/src/main/kotlin/com/animewitcher/AnimeWitcherPlugin.kt
+++ Animewitcher/src/main/kotlin/com/animewitcher/AnimeWitcherPlugin.kt
@@ -2,16 +2,10 @@ package com.animewitcher
 
 import com.lagradost.cloudstream3.plugins.CloudstreamPlugin
 import com.lagradost.cloudstream3.plugins.Plugin
-import com.lagradost.cloudstream3.plugins.TextSetting
 
 @CloudstreamPlugin
 class AnimeWitcherPlugin : Plugin() {
     override fun load() {
         registerMainAPI(AnimeWitcherProvider())
     }
-
-    // 🛠️ UI SETTINGS: Adds a text box in the extension settings to change the proxy host
-    override val settings = listOf(
-        TextSetting("proxy_host", "Proxy Worker Host", "issa-proxy.yazankal.workers.dev")
-    )
 }
