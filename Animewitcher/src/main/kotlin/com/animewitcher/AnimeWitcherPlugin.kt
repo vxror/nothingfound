0eee929920400c8f192c5ca428c09609cb035267
Leaf <xoxobrain@gmail.com>
2026-08-20 11:58:44 +0100

    Create AnimeWitcherPlugin.kt

---
+++ Animewitcher/src/main/kotlin/com/animewitcher/AnimeWitcherPlugin.kt
@@ -0,0 +1,11 @@
+package com.animewitcher
+
+import com.lagradost.cloudstream3.plugins.CloudstreamPlugin
+import com.lagradost.cloudstream3.plugins.Plugin
+
+@CloudstreamPlugin
+class AnimeWitcherPlugin : Plugin() {
+    override fun load() {
+        registerMainAPI(AnimeWitcherProvider())
+    }
+}
