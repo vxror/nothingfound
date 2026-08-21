be530580dda87e9f2c8cd5022bfa32e02ad40177
Leaf <xoxobrain@gmail.com>
2026-08-21 13:20:28 +0100

    Fix package name and simplify MegaProxy call

--- witanime/src/main/kotlin/com/witanime/WitAnimePlugin.kt
+++ witanime/src/main/kotlin/com/witanime/WitAnimePlugin.kt
@@ -1,16 +1,17 @@
-package com.cimanow
+package com.witanime
 
 import android.content.Context
 import com.lagradost.cloudstream3.plugins.CloudstreamPlugin
 import com.lagradost.cloudstream3.plugins.Plugin
 import com.witanime.MailruExtractor
+import com.witanime.MegaProxy
 import com.witanime.VideaExtractor
 import com.witanime.WitAnime
 
 @CloudstreamPlugin
 class WitAnimePlugin : Plugin() {
     override fun load(context: Context) {
-        com.witanime.MegaProxy.start()
+        MegaProxy.start()
         registerMainAPI(WitAnime())
         registerExtractorAPI(VideaExtractor())
         registerExtractorAPI(MailruExtractor())
