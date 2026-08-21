1136ed3948dd6cca08349431bd8381efa3546434
Leaf <xoxobrain@gmail.com>
2026-08-21 22:15:43 +0100

    Upgrade Java version and update dependencies
    Updated Java compatibility to version 17 and modified dependencies.

--- witanime/build.gradle.kts
+++ witanime/build.gradle.kts
@@ -1,3 +1,4 @@
+version = 1
 plugins {
     id("com.android.library")
     id("org.jetbrains.kotlin.android")
@@ -15,34 +16,27 @@ android {
         minSdk = 21
     }
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_1_8
-        targetCompatibility = JavaVersion.VERSION_1_8
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
     }
 }
 
-// 🚀 MOVED OUTSIDE android {} AND ADDED THE MAGIC FLAG
 kotlin {
     compilerOptions {
         jvmTarget.set(org.jetbrains.kotlin.gradle.dsl.JvmTarget.JVM_1_8)
         freeCompilerArgs.addAll(
             "-XXLanguage:+BreakContinueInInlineLambdas",
-            "-Xskip-metadata-version-check" // 🎯 THIS FIXES THE 2.4.0 METADATA CRASH
+            "-Xskip-metadata-version-check"
         )
     }
 }
 
 dependencies {
     val cloudstream by configurations
-    implementation("com.google.android.material:material:1.13.0")
-    implementation("androidx.browser:browser:1.9.0")
-    implementation("androidx.room:room-ktx:2.8.0")
-    testImplementation("junit:junit:4.13.2")
-    
     cloudstream("com.lagradost:cloudstream3:pre-release")
 
-    // 🆕 CRITICAL DEPENDENCIES REQUIRED BY YOUR CODE
-    implementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.16.0")
-    implementation("com.fasterxml.jackson.core:jackson-databind:2.16.0")
-    implementation("org.jsoup:jsoup:1.17.2")
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.8.1")
+    implementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.13.1")
+    implementation("com.github.Blatzar:NiceHttp:0.4.11")
+    implementation("org.jsoup:jsoup:1.18.3")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.10.2")
 }
