fe82af6d4a2bab6af5ce9c305798a7b7ad440b9b
Leaf <xoxobrain@gmail.com>
2026-08-21 21:09:43 +0100

    Modify build.gradle.kts for dependencies and namespace
    Updated Kotlin Gradle plugin version and adjusted namespace.

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,8 +7,8 @@ buildscript {
     repositories { google(); mavenCentral(); maven("https://jitpack.io") }
     dependencies {
         classpath("com.android.tools.build:gradle:8.7.3")
-        classpath("com.github.recloudstream:gradle:master-SNAPSHOT")
-        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:2.0.21")
+        classpath("com.github.recloudstream:gradle:-SNAPSHOT")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:2.3.0")
     }
 }
 
@@ -25,7 +25,7 @@ subprojects {
     cloudstream { setRepo(System.getenv("GITHUB_REPOSITORY") ?: "vxror/Witch") }
 
     android {
-        namespace = "com.witanime"
+        namespace = "com.animewitcher"
         defaultConfig {
             minSdk = 21
             compileSdkVersion(35)
@@ -38,12 +38,7 @@ subprojects {
         tasks.withType<KotlinJvmCompile> {
             compilerOptions {
                 jvmTarget.set(JvmTarget.JVM_1_8)
-                freeCompilerArgs.addAll(
-                    "-Xno-call-assertions",
-                    "-Xno-param-assertions",
-                    "-Xno-receiver-assertions",
-                    "-Xskip-metadata-version-check"   // ← THE ONLY LINE I ADDED. Fixes metadata 2.4.0 vs 2.0.x
-                )
+                freeCompilerArgs.addAll("-Xno-call-assertions", "-Xno-param-assertions", "-Xno-receiver-assertions")
             }
         }
     }
