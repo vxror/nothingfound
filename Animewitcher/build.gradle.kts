69a945672c5ae328ac01d37f123ca09543da6da6
Leaf <xoxobrain@gmail.com>
2026-08-21 21:57:39 +0100

    Configure Kotlin options and add dependencies
    Added Kotlin compiler options and new dependencies for the settings UI.

--- Animewitcher/build.gradle.kts
+++ Animewitcher/build.gradle.kts
@@ -1,9 +1,16 @@
 version = 1
 
+kotlin {
+    compilerOptions {
+        jvmTarget.set(org.jetbrains.kotlin.gradle.dsl.JvmTarget.JVM_1_8)
+        freeCompilerArgs.addAll(
+            "-XXLanguage:+BreakContinueInInlineLambdas",
+            "-Xskip-metadata-version-check"
+        )
+    }
+}
+
 dependencies {
-    // ... [KEEP ALL YOUR EXISTING DEPENDENCIES HERE] ...
-    
-    // 🆕 ADD THESE 4 LINES FOR THE SETTINGS UI:
     implementation("androidx.appcompat:appcompat:1.7.0")
     implementation("com.google.android.material:material:1.12.0")
     implementation("androidx.preference:preference-ktx:1.2.1")
