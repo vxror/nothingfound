35f25ececeb1e6ba9442d4c1780c19914cafaf36
Leaf <xoxobrain@gmail.com>
2026-08-22 05:52:15 +0100

    Rename package from com.witanime to com.cimanow

--- witanime/src/main/kotlin/com/witanime/WitAnimePlugin.kt
+++ witanime/src/main/kotlin/com/witanime/WitAnimePlugin.kt
@@ -1,8 +1,11 @@
-package com.witanime
+package com.cimanow
 
 import android.content.Context
 import com.lagradost.cloudstream3.plugins.CloudstreamPlugin
 import com.lagradost.cloudstream3.plugins.Plugin
+import com.witanime.WitAnime
+import com.witanime.VideaExtractor
+import com.witanime.MailruExtractor
 
 @CloudstreamPlugin
 class WitAnimePlugin : Plugin() {
