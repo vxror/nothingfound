af555f4df639a12bc69dc0372f7fa52366069bdd
Leaf <xoxobrain@gmail.com>
2026-08-22 06:37:26 +0100

    Add new dependencies to build.gradle.kts

--- Animewitcher/build.gradle.kts
+++ Animewitcher/build.gradle.kts
@@ -15,4 +15,8 @@ dependencies {
     implementation("com.google.android.material:material:1.12.0")
     implementation("androidx.preference:preference-ktx:1.2.1")
     implementation("androidx.fragment:fragment-ktx:1.8.2")
+    implementation("androidx.browser:browser:1.9.0")
+    implementation("androidx.room:room-ktx:2.8.0")
+    implementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.16.0")
+    implementation("com.fasterxml.jackson.core:jackson-databind:2.16.0")
 }
