580599d8db4f565f4668163f31cc50b8d178be8d
Leaf <xoxobrain@gmail.com>
2026-08-27 12:48:09 +0100

    Update plugin management repositories in settings.gradle.kts

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,4 +1,14 @@
+pluginManagement {
+    repositories {
+        gradlePluginPortal()
+        google()
+        mavenCentral()
+        maven("https://jitpack.io")
+    }
+}
+
 rootProject.name = "nothingfound"
+
 include(":Animewitcher")
 include(":witanime")
 include(":vxrux")
