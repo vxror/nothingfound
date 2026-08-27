da49c35db8b7e1e939bad5d7972fb122cf12cb5d
Leaf <xoxobrain@gmail.com>
2026-08-27 17:13:55 +0100

    Delete vxrux directory

--- vxrux/src/main/kotlin/com/vxrux/extractors/VidhideExtractor.kt
+++
@@ -1,7 +0,0 @@
-package com.vxrux.extractors
-
-open class VidhideExtractor : VidHidePro() {
-    override var name = "VidHide"
-    override var mainUrl = "https://vidhide.com"
-    override val requiresReferer = false
-}
