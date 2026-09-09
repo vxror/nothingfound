3207baa34a861336472bb4795422a4da757f93b5
Leaf <xoxobrain@gmail.com>
2026-09-09 13:19:06 +0100

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,7 +7,7 @@ buildscript {
     repositories { google(); mavenCentral(); maven("https://jitpack.io") }
     dependencies {
         classpath("com.android.tools.build:gradle:8.7.3")
-        classpath("com.github.recloudstream:gradle:master-SNAPSHOT")
+        classpath("com.github.recloudstream:gradle:1.0.2")
         classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:2.1.0")
     }
     
